5a9b0d4f2e82fcfe49a6be120dd1f12ebc6d154d
Revashing <101721332+Revashing@users.noreply.github.com>
2022-06-21 09:28:11 +0300

    nothing special

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from bboard.models import FirstModel
 
+
 def home(request):
     return HttpResponse('<h1>Hello world</h1>')
 
 def inde(request):
-    s = 'Список объявлений\r\n\r\n\r\n'
-    for spob in FirstModel.objects.order_by('-published'):
-        s += spob.title + '\r\n' + spob.content + '\r\n\r\n'
-    return HttpResponse(s, content_type='text/plain; charset=utf-8')
+    bbs = FirstModel.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs': bbs})
