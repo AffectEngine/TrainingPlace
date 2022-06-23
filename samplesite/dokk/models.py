33d40ff857833ff9cb2a2b260c08be6e087350f6
Revashing <101721332+Revashing@users.noreply.github.com>
2022-06-23 13:52:46 +0300

    nothing special

--- samplesite/dokk/models.py
+++ samplesite/dokk/models.py
@@ -17,6 +17,9 @@ class SecondModel(models.Model):
 class Rubrictwo(models.Model):
     name = models.CharField(max_length=20, db_index=True, verbose_name='Название')
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name = 'Рубрика'
         verbose_name_plural = 'Рубрики'
