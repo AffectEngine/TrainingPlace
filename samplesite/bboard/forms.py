cc5d92ac7e02f11a21b1847c37ceefbe669bc335
Revashing <101721332+Revashing@users.noreply.github.com>
2022-06-23 17:58:48 +0300

    nothing special

--- samplesite/bboard/forms.py
+++ samplesite/bboard/forms.py
@@ -4,4 +4,4 @@ from .models import FirstModel
 class FirstModelForm(ModelForm):
     class Meta:
         model = FirstModel
-        fields = ('title', 'content', ' price', 'rubric')
+        fields = ('title', 'content', 'price', 'rubric')
--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import inde, by_rubric
+from .views import inde, by_rubric, FirstModelCreateView
 
 
 urlpatterns = [
+    path('add/', FirstModelCreateView.as_view(), name='add'),
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('', inde, name='inde'),
 ]
