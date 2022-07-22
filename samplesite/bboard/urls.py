37a8cfc899237a128c2716e5c0da20f22270fe29
Revashing <101721332+Revashing@users.noreply.github.com>
2022-07-22 16:42:56 +0300

    Added new way to move between Rubrics by it's numbers (path)

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import inde, by_rubric, FirstModelCreateView, PersonView, FirstModelByRubricView
+from .views import inde, FirstModelCreateView, PersonView, FirstModelByRubricView
 
 app_name = 'bboard'
 
 urlpatterns = [
     path('add/', FirstModelCreateView.as_view(), name='add'),
-    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+#    path('<int:rubric_id>/', by_rubric, name='by_rubric'), (ОБЫЧНЫЙ ПУТЬ ВЫВОДА РУБРИК ПО ИХ КЛЮЧУ (ЗАМЕНЕНО as_view FirstModelByRubricView)
     path('', inde, name='inde'),
     path('users/<int:id>/<str:name>/', PersonView.as_view(), name='bboard'),
-    path('rubr/', FirstModelByRubricView.as_view(), name='rubr')
+    path('<int:rubric_id>/', FirstModelByRubricView.as_view(), name='by_rubric')
 ]
