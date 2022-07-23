eb3e50950dcedb63ef9341c43196d29b73a124cf
Revashing <101721332+Revashing@users.noreply.github.com>
2022-07-23 15:27:23 +0300

    New url for DetailView test

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import inde, FirstModelCreateView, PersonView, FirstModelByRubricView
+from .views import inde, FirstModelCreateView, PersonView, FirstModelByRubricView, FirstModelDetailView
 
 app_name = 'bboard'
 
@@ -8,5 +8,6 @@ urlpatterns = [
 #    path('<int:rubric_id>/', by_rubric, name='by_rubric'), (ОБЫЧНЫЙ ПУТЬ ВЫВОДА РУБРИК ПО ИХ КЛЮЧУ (ЗАМЕНЕНО as_view FirstModelByRubricView)
     path('', inde, name='inde'),
     path('users/<int:id>/<str:name>/', PersonView.as_view(), name='bboard'),
-    path('<int:rubric_id>/', FirstModelByRubricView.as_view(), name='by_rubric')
+    path('<int:rubric_id>/', FirstModelByRubricView.as_view(), name='by_rubric'),
+    path('detail/<int:pk>/', FirstModelDetailView.as_view(), name='detail'),
 ]
