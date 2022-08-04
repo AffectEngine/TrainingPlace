84e7ef6e8fde25c2e6a7567a116dad5d961037e2
Revashing <101721332+Revashing@users.noreply.github.com>
2022-08-04 18:50:51 +0300

    New ways to create Model-Forms Rubric model-form Register model-form

--- samplesite/bboard/forms.py
+++ samplesite/bboard/forms.py
@@ -1,5 +1,10 @@
-from django.forms import ModelForm
-from .models import FirstModel, Person
+from django.forms import ModelForm, modelform_factory, DecimalField
+from django.forms.widgets import Select
+from django import forms
+
+from .models import FirstModel, Person, Rubric
+
+
 # from urllib import request
 # from django.core.files.base import ContentFile
 # from django.utils.text import slugify
@@ -15,3 +20,24 @@ class PersonForm(ModelForm):
     class Meta:
         model = Person
         fields = ('name', 'second_name', 'skin_color', 'sex', 'age', 'mail', 'git')
+
+
+class FirstModelFullForm(forms.ModelForm):
+    price = forms.DecimalField(label='Цена', decimal_places=2)
+    rubric = forms.ModelChoiceField(queryset=Rubric.objects.all(), label='Рубрика',
+                                    help_text='Не забудьте задать рубрику!',
+                                    widget=forms.widgets.Select(attrs={'size': 8}))
+
+    class Meta:
+        model = FirstModel
+        fields = ('title', 'content', 'price', 'rubric')
+        labels = {'title': 'Название товара'}
+
+
+class RegisterPersonForm(forms.ModelForm):
+    password1 = forms.CharField(label='Пароль')
+    password2 = forms.CharField(label='Повторите пароль')
+
+    class Meta:
+        model = Person
+        fields = ('name', 'second_name', 'skin_color', 'sex', 'age', 'password1', 'password2', 'mail', 'git')
