79c7da3afb8627c8f51620b4e2960d4b4482edfa
Revashing <101721332+Revashing@users.noreply.github.com>
2022-08-22 22:07:09 +0300

    NEW path for formset(edit rubrics)

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import inde, edit, delete, contact, rubrics, PersonRegView, FirstModelByRubricViewL, FirstModelDetailView, FirstModelAddView, \
+from .views import inde, edit, delete, contact, rubrics, first_model_inline_formset, PersonRegView, \
+    FirstModelByRubricViewL, FirstModelDetailView, FirstModelAddView, \
     FirstModelRedirectView, RegisterPersonForm, PersonDisplayView
 
 app_name = 'bboard'
 
 urlpatterns = [
-    #path('<int:rubric_id>/', by_rubric, name='by_rubric'), (ОБЫЧНЫЙ ПУТЬ ВЫВОДА РУБРИК ПО ИХ КЛЮЧУ (ЗАМЕНЕНО as_view FirstModelByRubricView),
-    #path('<int:rubric_id>/', FirstModelByRubricView.as_view(), name='by_rubric'), (Замены урла TemplateView на ListView),
+    # path('<int:rubric_id>/', by_rubric, name='by_rubric'), (ОБЫЧНЫЙ ПУТЬ ВЫВОДА РУБРИК ПО ИХ КЛЮЧУ (ЗАМЕНЕНО as_view FirstModelByRubricView),
+    # path('<int:rubric_id>/', FirstModelByRubricView.as_view(), name='by_rubric'), (Замены урла TemplateView на ListView),
     # path('edit/<int:pk>/', FirstModelEditView.as_view(), name='edit'), (Замена UpdateView на к-ф edit для правки записи по pk из URL)
     # path('delete/<int:pk>/', FirstModelDeleteView.as_view(), name='rub-delete'), (Замена DeleteView на к-ф delete для удаления записи по pk из URL)
     path('', inde, name='inde'),
@@ -14,6 +15,7 @@ urlpatterns = [
     path('add/', FirstModelAddView.as_view(), name='add'),
     path('view/', contact, name='contact'),
     path('rubric/', rubrics, name='rubrics'),
+    path('rubricsID/<int:rubric_id>/', first_model_inline_formset, name='rubricsID'),
 
     path('<int:rubric_id>/', FirstModelByRubricViewL.as_view(), name='by_rubric'),
     path('detail/<int:pk>/', FirstModelDetailView.as_view(), name='detail'),
