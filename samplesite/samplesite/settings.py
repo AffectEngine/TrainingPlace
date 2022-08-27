3354e10b9b5fd1f1484e142513c58005f59cbdfd
Revashing <101721332+Revashing@users.noreply.github.com>
2022-08-27 17:09:13 +0300

    NEW hidden() EMAIL settings

--- samplesite/samplesite/settings.py
+++ samplesite/samplesite/settings.py
@@ -125,3 +125,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGOUT_REDIRECT_URL = 'bboard:index'
 
 DEFAULT_FROM_EMAIL = 'passwordChanger@gmail.com'
+
+#SMTP Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'revvashing@gmail.com'
+EMAIL_HOST_PASSWORD = 'umpcuxbzfffdtcan'
